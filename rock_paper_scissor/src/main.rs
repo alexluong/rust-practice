771460d9b6697c70f218d4bdfcf7247fc94200bb
Alex Luong <alex@alexluong.com>
2019-11-26 01:20:59 -0600

    feat(rock_paper_scissor): yay

---
+++ rock_paper_scissor/src/main.rs
@@ -0,0 +1,91 @@
+use rand::Rng;
+use std::io;
+use std::io::Write;
+
+const ROCK: &str = "rock";
+const PAPER: &str = "paper";
+const SCISSOR: &str = "scissor";
+
+const WIN: i32 = 1;
+const DRAW: i32 = 0;
+const LOSE: i32 = -1;
+
+fn generate_computer_move() -> &'static str {
+    let computer_move = rand::thread_rng().gen_range(1, 3);
+
+    match computer_move {
+        1 => return ROCK,
+        2 => return PAPER,
+        3 => return SCISSOR,
+        _ => panic!("Error generating computer move!"), // impossible
+    }
+}
+
+fn validate_move(user_move: &str) -> bool {
+    if user_move == ROCK || user_move == PAPER || user_move == SCISSOR {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+fn evaluate_move(user_move: &str, computer_move: &str) -> i32 {
+    if user_move == ROCK {
+        if computer_move == ROCK {
+            return DRAW;
+        } else if computer_move == PAPER {
+            return LOSE;
+        } else {
+            return WIN;
+        }
+    } else if user_move == PAPER {
+        if computer_move == ROCK {
+            return WIN;
+        } else if computer_move == PAPER {
+            return DRAW;
+        } else {
+            return LOSE;
+        }
+    } else {
+        if computer_move == ROCK {
+            return LOSE;
+        } else if computer_move == PAPER {
+            return WIN;
+        } else {
+            return DRAW;
+        }
+    }
+}
+
+fn announce_result(result: i32) {
+    match result {
+        1 => println!("You win!"),
+        0 => println!("It's a draw"),
+        -1 => println!("You lose :("),
+        _ => panic!("Error announcing result!"), // impossible
+    }
+}
+
+fn main() {
+    let computer_move = generate_computer_move();
+    // println!("Computer move: {}", computer_move);
+
+    loop {
+        print!("Your move: ");
+        io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line.");
+
+        let user_move = input.trim();
+
+        if validate_move(user_move) {
+            let result = evaluate_move(user_move, computer_move);
+            announce_result(result);
+            break;
+        }
+    }
+}
